757c3bd5b0003559c25057f4ce2103c7b8c10a0d
Joseph Ramsay <jfr573@cs.bham.ac.uk>
2020-04-09 16:13:17 +0100

    make request to server

--- src/data/PupilStore.js
+++ src/data/PupilStore.js
@@ -1,4 +1,6 @@
+import $ from 'jquery';
 import Immutable from 'immutable';
+import Promise from 'promise';
 import {ReduceStore} from 'flux/utils';
 import PupilActionTypes from './PupilActionTypes';
 import PupilDispatcher from './PupilDispatcher';
@@ -14,6 +16,26 @@ class PupilStore extends ReduceStore {
             textBox: '',
             time: 0,
         };
+    }
+
+    askForGame(pupilID) {
+        return new Promise((resolve, reject) => {
+            $.get("cgi-bin/askForGame.py",
+                  {"pupilID": pupilID,
+                   "date": new Date().getTime(),
+                  },
+                  function(data) {
+                      console.log("asked");
+                      success = data.split(":");
+                      if (success[0]=="Found") {
+                          console.log('yee');
+                          resolve(success[1]);
+                      } else {
+                          console.log('nein');
+                          resolve(undefined);
+                      }
+                  });
+        });
     }
 
     reduce(state, action) {
@@ -34,9 +56,14 @@ class PupilStore extends ReduceStore {
             };
 
         case PupilActionTypes.TICK:
+            if (state.textBox && state.time % 3 == 0) {
+                this.askForGame(state.textBox).then((t) => {
+                    console.log("done");
+                });
+            }
             return {
                 listItems: state.listItems,
-                textBox: action.text,
+                textBox: state.textBox,
                 time: state.time + 1,
             };
 
